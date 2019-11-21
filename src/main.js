f846cebd1585f8533e215623b04d44253835e18c
Student <example@example.com>
2019-11-21 11:12:52 -0800

    add working code with promise method

--- src/main.js
+++ src/main.js
@@ -2,3 +2,31 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function(){
+  $('form').submit(function(event){
+    event.preventDefault();
+    let numberInput = $('input#numberInput').val();
+
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.ekata.com/3.0/phone.json?api_key=${process.env.API_KEY}&phone.country_hint=US&phone=${numberInput}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $(".output").text(`The person you are creeping on is named ${body.belongs_to[0].name}`);
+    }, function(error) {
+      $('.showErrors').text(`you suck: ${error.message}`);
+    });
+  });
+});
